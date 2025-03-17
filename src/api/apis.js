7e481eaf9cbd4bb981069ba6895db9688acca17d
hacergkk <hacergk144@gmail.com>
2025-03-17 13:10:57 +0300

    Retrieved SubNode and camera values.

--- src/api/apis.js
+++ src/api/apis.js
@@ -37,3 +37,41 @@ export const getNode = async () => {
         alert("Error: " + error.message);
     }
 };
+
+export const getSubNode = async () => {
+    const token = await getToken();
+    try {
+        const response = await fetch("https://pts.mangobulut.com/apib/sub-node", {
+            method: "GET",
+            headers: {
+                "ContentType": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+
+        });
+
+        return await response.json();
+
+    } catch (error) {
+        alert("Error: " + error.message);
+    }
+};
+
+export const getCamera = async () => {
+    const token = await getToken();
+    try {
+        const response = await fetch("https://pts.mangobulut.com/apib/camera/record-capture-time?order=ASC", {
+            method: "GET",
+            headers: {
+                "ContentType": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+
+        });
+
+        return await response.json();
+
+    } catch (error) {
+        alert("Error: " + error.message);
+    }
+};
--- src/pages/FormMotorcycle/FormMotorcycle.jsx
+++ src/pages/FormMotorcycle/FormMotorcycle.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ComboBox from "../../components/ComboboxForm";
 //import Node from "../../CameraList/node";
-import { getNode } from "../../api/apis";
+import { getNode, getSubNode, getCamera } from "../../api/apis";
 // import SubNode from "../../CameraList/subnode";
 // import Camera from "../../CameraList/camera";
 import ProcessType from "../../CameraList/processtype";
@@ -13,9 +13,12 @@ const FormMotorcycle = ({ setIsAuthenticated }) => {
 
     const navigate = useNavigate();
     const [nodeOptions, setNodeOptions] = useState([]);
+    const [subNodeOptions, setSubNodeOptions] = useState([]);
+    const [subCamera, setCameraOptions] = useState([]);
 
     useEffect(() => {
         document.body.className = "formmotorcycle-page";
+
         const fetchNodes = async () => {
             try {
                 const nodes = await getNode();
@@ -30,7 +33,37 @@ const FormMotorcycle = ({ setIsAuthenticated }) => {
             }
         };
 
+        const fetchSubNodes = async () => {
+            try {
+                const subNodes = await getSubNode();
+                if (subNodes && Array.isArray(subNodes)) {
+                    setSubNodeOptions(subNodes.map(subNodes => subNodes.name));
+                } else {
+                    setSubNodeOptions([]);
+                }
+            } catch (error) {
+                //hata-alert çağrılacak
+                setSubNodeOptions([]);
+            }
+        };
+
+        const fetchCamera = async () => {
+            try {
+                const subCamera = await getCamera();
+                if (subCamera && Array.isArray(subCamera)) {
+                    setCameraOptions(subCamera.map(subCamera => subCamera.name));
+                } else {
+                    setCameraOptions([]);
+                }
+            } catch (error) {
+                //hata-alert çağrılacak
+                setCameraOptions([]);
+            }
+        };
+
         fetchNodes();
+        fetchSubNodes();
+        fetchCamera();
     }, []);
     document.title = "Motorcycle Process";
 
@@ -47,8 +80,8 @@ const FormMotorcycle = ({ setIsAuthenticated }) => {
             <div className="form-container">
 
                 <ComboBox title="Node" options={nodeOptions} multipleChoice={true} />
-                <ComboBox title="Sub Node" options={nodeOptions} multipleChoice={true} />
-                <ComboBox title="Camera" options={nodeOptions} multipleChoice={true} />
+                <ComboBox title="Sub Node" options={subNodeOptions} multipleChoice={true} />
+                <ComboBox title="Camera" options={subCamera} multipleChoice={true} />
                 <ComboBox title="Process Type" options={ProcessType} />
 
             </div>
